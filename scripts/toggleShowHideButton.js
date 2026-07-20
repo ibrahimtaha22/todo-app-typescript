655299ba2e6ebb6771119c4eef67d00a1173aaed
ibrahimtaha22 <ibrahimtaha4329@gmail.com>
2026-07-20 11:22:33 +0300

    feat: save show/hide tasks state to localstorage

--- scripts/toggleShowHideButton.js
+++ scripts/toggleShowHideButton.js
@@ -1,10 +1,21 @@
 import { TaskList, showHideTaskButton } from "./elements";
 import { hasCompletedTask } from "./hasCompletedTask";
+import { saveToDB } from "./saveToDB";
 
 export const toggleShowHideButton = () => {
   if (hasCompletedTask()) {
     showHideTaskButton.classList.toggle("TaskList__link--isActive");
     TaskList.classList.toggle("TaskList__list--hideCompleted");
+
+    const isShowButton = showHideTaskButton.classList.contains(
+      "TaskList__link--isActive",
+    );
+    const isHideCompleted = TaskList.classList.contains(
+      "TaskList__list--hideCompleted",
+    );
+
+    saveToDB("isShowButton", isShowButton);
+    saveToDB("isHideCompleted", isHideCompleted);
   } else {
     return;
   }
