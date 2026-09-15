104841a6728bd51d1e2501c4d3169e7333792d8a
ibrahimtaha22 <ibrahimtaha4329@gmail.com>
2026-09-15 18:34:17 +0300

    chore: set up TypeScript environment

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        localStorage: "readonly",
+        console: "readonly",
+        module: "readonly",
+        require: "readonly",
+        confirm: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+];
